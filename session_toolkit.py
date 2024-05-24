b9cc0de820ff01a1e78d674be671dbed7411dc72
Kai <koreadeep19@gmail.com>
2024-05-24 19:36:18 +0900

    ADD: session 관리 및 api 추가

---
+++ session_toolkit.py
@@ -0,0 +1,151 @@
+import requests
+import numpy as np
+import cv2
+from PIL import Image
+import io
+from requests_toolbelt import MultipartEncoder
+
+def login_and_create_session(url="https://api.polyground.info/v1/auth/login", id="admin", pw="admin1234"):
+    
+    """
+    사용자 ID와 비밀번호를 이용해 로그인하고, 반환된 토큰과 쿠키를 세션 헤더에 저장.
+
+    Args:
+    - url (str): 로그인 API의 URL 주소
+    - id (str): 사용자 ID
+    - pw (str): 사용자 비밀번호
+
+    Returns:
+    - session (requests.Session): 인증 정보가 포함된 세션 객체
+    """
+    session = requests.Session()
+    data = {
+        "loginId": id,
+        "password": pw,
+        "authType": "POLYGROUND"
+    }
+    response = session.post(url, json=data)
+    
+    response.raise_for_status()
+    
+    token = response.json().get("token")
+    cookies = response.cookies
+    cookie_header = '; '.join([f"{key}={value}" for key, value in cookies.items()])
+
+    session.headers.update({'Authorization': f'Bearer {token}', 'Cookie': cookie_header})
+    print("Successfully logged in")
+        
+    return session
+
+
+def reissue(session, url="https://api.polyground.info/v1/auth/reissue"):
+    """
+    세션으로 토큰 재발급 요청.
+    새로운 토큰으로 세션의 인증 헤더를 업데이트. (기존 토큰 갱신)
+
+    Args:
+    - session (requests.Session): 현재 유효한 세션 객체
+    - url (str): 토큰 재발급 API의 URL 주소
+
+    Returns:
+    - session (requests.Session): 업데이트된 세션 객체
+    """
+    res = session.patch(url)
+    res.raise_for_status()
+    token = res.json().get("token")
+    session.headers.update({'Authorization': f'Bearer {token}'})
+
+def reissue_decorator(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 401:
+                session = args[0]
+                reissue(session)
+                return func(*args, **kwargs)
+            else:
+                raise e
+    return wrapper
+
+@reissue_decorator
+def get_resource_content_public(session, resource_id, url="https://api.play.polyground.ai/v1/resource/public"):
+    """
+    public resource 에 ID를 통해 파일을 바이너리 형태로 받음.
+
+    Args:
+    - session (requests.Session): 인증 정보가 포함된 세션 객체
+    - resource_id (str): 고유 ID
+    - url (str): public resource API의 URL 주소
+
+    Returns:
+    - content (bytes): 파일의 바이너리 데이터
+    """
+    resource_url = f"{url}/{resource_id}"
+    res = session.get(resource_url)
+    res.raise_for_status()
+    return res.content
+
+@reissue_decorator
+def upload_resource_public(session, name, file_buffer, mime_type="image/jpeg", url="https://api.play.polyground.ai/v1/resource/public"):
+    """
+    파일을 멀티파트 형식으로 서버에 업로드.
+    파일의 이름, 데이터, 타입을 이용하여 멀티파트 요청을 구성하고 전송.
+
+    Args:
+    - session (requests.Session): 인증 정보가 포함된 세션 객체
+    - name (str): 업로드할 파일의 이름
+    - file_buffer (bytes): 파일 데이터
+    - mime_type (str): 파일의 MIME 타입 (기본값은 "image/jpeg")
+    - url (str): 파일 업로드 API의 URL 주소
+
+    Returns:
+    - res (requests.Response): 요청 결과를 담은 응답 객체
+    """
+    m = MultipartEncoder(
+        fields={'file': (name, file_buffer, mime_type)}
+    )
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': m.content_type
+    }
+    res = session.post(url, data=m, headers=headers)
+    return res
+
+def read_image(content):
+    """
+    바이너리 형태의 이미지 데이터를 NumPy 배열로 변환하여 이미지로 읽음.
+
+    Args:
+    - content (bytes): 이미지 파일의 바이너리 데이터
+
+    Returns:
+    - img (numpy.ndarray): 변환된 이미지 데이터
+    """
+    nparr = np.frombuffer(content, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
+def array_to_buffer(array):
+    image = Image.fromarray(array)
+
+    # 이미지를 바이트 스트림으로 변환
+    buffered = io.BytesIO()
+    image.save(buffered, format="JPEG")
+    return buffered.getvalue()
+
+if __name__ == "__main__":
+    session = login_and_create_session()
+    content = get_resource_content_public(session, 1)
+    img = read_image(content)
+    
+
+    # 이미지 업로드 예시
+    res = upload_resource_public(session,"1c0e6d73176a50e81f5d96c0de73547a.jpg", array_to_buffer(img), mime_type="image/jpeg")
+    
+    # 토큰 갱신
+    reissue(session)
+    
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
