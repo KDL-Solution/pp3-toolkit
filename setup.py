981e6c6ea3b77846a8e14f1fc3db092a6240e7e4
Kai <koreadeep19@gmail.com>
2024-06-05 15:53:36 +0900

    TEST:

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import setuptools
 
-def load_requirements(filename='requirements.txt'):
-    """Load requirements from a requirements file."""
-    with open(filename, 'r') as file:
-        requirements = file.read().splitlines()
-        requirements = [r.strip() for r in requirements if r.strip() and not r.startswith('#')]
-    return requirements
+install_requires = [
+    'requests==2.32.2',
+    'pillow==10.3.0',
+    'requests_toolbelt==1.0.0'
+]
 
 # 안전하게 README 파일 읽기
 def read_long_description():
@@ -17,15 +16,15 @@ def read_long_description():
     
 setuptools.setup(
     name="pp3", # 모듈 이름
-    version="0.0.1", # 버전
-    packages = setuptools.find_packages(), # 모듈을 자동으로 찾아줌
+    version="0.1.1", # 버전
+    packages = ['pp3'], # 패키지 디렉토리
     author="Kai", # 제작자
     author_email="koreadeep19@gmail.com", # contact
     description="koreadep play-polyground api toolkit", # 모듈 설명
     long_description=read_long_description(), # README.md에 보통 모듈 설명을 해놓는다.
     long_description_content_type="text/markdown",
     url="https://github.com/KDL-Solution/PP3Toolkit",
-    install_requires=load_requirements(),
+    install_requires=install_requires,
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -35,7 +34,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10'
     ],
-    package_data={'pp3': ['LICENSE.txt', 'requirements.txt']},
-    include_package_data=True,
     python_requires=">=3.8"
 )
